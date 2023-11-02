a4bf3f9338ea23c54a93b2719a918af3a08c736a
Liu XingHao <146856742+xinghaoliu@users.noreply.github.com>
2023-11-02 23:53:32 +0800

    Update Pid_controller.h

--- PID_src/Pid_controller.h
+++ PID_src/Pid_controller.h
@@ -3,14 +3,14 @@
 
 typedef struct
 {
-	double kp,ki,kd;
-	double error,prev_error;
+	double kp, ki, kd;
+	double error, prev_error;
 	double integral;
-	double output,output_max;
+	double output, output_max;
 }PIDController;
 
 //PID导出函数
-void PID_Init(PIDController *pid,double kp,double ki,double kd,double output_max);
-double PID_Iterate(PIDController *pid,double target,double actual,double dt);
+void PID_Init(PIDController *pid, double kp, double ki, double kd, double output_max);
+double PID_Iterate(PIDController *pid, double target, double actual, double dt);
 
 #endif
