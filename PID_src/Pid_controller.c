f49baed02ac187453c0c94c0ed25a7239ef1b170
Liu XingHao <146856742+xinghaoliu@users.noreply.github.com>
2023-11-03 00:36:12 +0800

    Update Pid_controller.c

--- PID_src/Pid_controller.c
+++ PID_src/Pid_controller.c
@@ -19,7 +19,7 @@ double PID_Iterate(PIDController *pid, double target, double actual, double dt)
 	
 	double derivative = (pid->error - pid->prev_error) / dt;
 
-	pid->output = pid->error * pid->kp + pid->integral * pid->kp +derivative * pid->kd;
+	pid->output = pid->error * pid->kp + pid->integral * pid->ki +derivative * pid->kd;
 	
 	if(pid->output > pid->output_max) pid->output = pid->output_max;
     	if(pid->output < -pid->output_max) pid->output = -pid->output_max;
