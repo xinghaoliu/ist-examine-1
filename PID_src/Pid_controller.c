e0f15ba39655d20afb807eec429d295f8714b834
Liu XingHao <146856742+xinghaoliu@users.noreply.github.com>
2023-11-02 23:31:54 +0800

    Update Pid_controller.c

--- PID_src/Pid_controller.c
+++ PID_src/Pid_controller.c
@@ -2,27 +2,27 @@
 
 void PID_Init(PIDController *pid,double kp,double ki,double kd,double output_max)
 {
-	pid->kp=kp;
-	pid->ki=ki;
-	pid->kd=kd;
-	pid->integral=0;
-	pid->error=0;
-	pid->output_max=output_max;
+	pid->kp = kp;
+	pid->ki = ki;
+	pid->kd = kd;
+	pid->integral = 0;
+	pid->error = 0;
+	pid->output_max = output_max;
 }
 
 double PID_Iterate(PIDController *pid,double target,double actual,double dt)
 {
-	pid->prev_error=pid->error;
-	pid->error=target-actual;
+	pid->prev_error = pid->error;
+	pid->error = target - actual;
 	
-	pid->integral+=pid->error*dt;
+	pid->integral += pid->error * dt;
 	
-	double derivative=(pid->error-pid->prev_error)*dt;
+	double derivative = (pid->error - pid->prev_error) / dt;
 
 	pid->output = pid->error * pid->kp + pid->integral * pid->kp +derivative * pid->kd;
 	
-	if(pid->output > pid->output_max) pid->output=pid->output_max;
-    	if(pid->output < -pid->output_max) pid->output=-pid->output_max;
+	if(pid->output > pid->output_max) pid->output = pid->output_max;
+    	if(pid->output < -pid->output_max) pid->output = -pid->output_max;
 	
 	return pid->output;
 }
