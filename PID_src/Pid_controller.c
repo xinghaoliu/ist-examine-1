bd7a7294eecbf3a5e890309f1e5b0cbd6f794204
Liu XingHao <146856742+xinghaoliu@users.noreply.github.com>
2023-11-02 23:53:04 +0800

    Update Pid_controller.c

--- PID_src/Pid_controller.c
+++ PID_src/Pid_controller.c
@@ -1,6 +1,6 @@
 #include "Pid_controller.h"
 
-void PID_Init(PIDController *pid,double kp,double ki,double kd,double output_max)
+void PID_Init(PIDController *pid, double kp, double ki, double kd, double output_max)
 {
 	pid->kp = kp;
 	pid->ki = ki;
@@ -10,7 +10,7 @@ void PID_Init(PIDController *pid,double kp,double ki,double kd,double output_max
 	pid->output_max = output_max;
 }
 
-double PID_Iterate(PIDController *pid,double target,double actual,double dt)
+double PID_Iterate(PIDController *pid, double target, double actual, double dt)
 {
 	pid->prev_error = pid->error;
 	pid->error = target - actual;
