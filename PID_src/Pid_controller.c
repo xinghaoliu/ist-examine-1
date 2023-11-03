56a3790e79d57071d2358019955b762139d92485
Liu XingHao <146856742+xinghaoliu@users.noreply.github.com>
2023-11-03 15:20:11 +0800

    Update Pid_controller.c

--- PID_src/Pid_controller.c
+++ PID_src/Pid_controller.c
@@ -1,6 +1,14 @@
 #include "Pid_controller.h"
 
-// 初始化函数，设置kp、ki、kd参数
+/**
+  * @brief PID结构体初始化函数
+  * @param pid：指向PIDController结构体的指针
+  * @param kp：比例参数
+  * @param ki：积分参数
+  * @param kd：微分参数
+  * @param output_max：输出限幅值，PID算法的输出最大值不会大于此值，最小值不会小于此值的负数
+  * @retval 无
+  */
 void PID_Init(PIDController *pid, double kp, double ki, double kd, double output_max)
 {
 	pid->kp = kp;
@@ -11,6 +19,13 @@ void PID_Init(PIDController *pid, double kp, double ki, double kd, double output
 	pid->output_max = output_max;
 }
 
+/**
+  * @brief PID算法函数
+  * @param pid：指向PIDController结构体的指针
+  * @param target：系统的目标值
+  * @param feedback：系统的反馈值
+  * @retval PID算法的输出值
+  */
 double PID_Iterate(PIDController *pid, double target, double feedback)
 {
 	pid->prev_error = pid->error;				
